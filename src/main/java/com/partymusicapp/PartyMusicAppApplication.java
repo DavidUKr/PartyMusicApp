db02cc4828838080ed4c298819e7ee42cf43c647
DavidUKr <davidkrinurs@gmail.com>
2023-10-19 22:21:54 +0300

    first commit + some docs

---
+++ src/main/java/com/partymusicapp/PartyMusicAppApplication.java
@@ -0,0 +1,13 @@
+package com.partymusicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyMusicAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartyMusicAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/partymusicapp/PartyMusicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partymusicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyMusicAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
