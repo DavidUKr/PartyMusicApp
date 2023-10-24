8f58d81b466cd20a691ca065316f1e0a14ac4853
DavidUKr <davidkrinurs@gmail.com>
2023-10-25 01:50:21 +0300

    added service interfaces

--- src/main/java/com/partymusicapp/service/UserService.java
+++ src/main/java/com/partymusicapp/service/UserService.java
@@ -1,7 +1,9 @@
 package com.partymusicapp.service;
 
 import com.partymusicapp.models.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserService {
     void registerUser(User user);
 }
