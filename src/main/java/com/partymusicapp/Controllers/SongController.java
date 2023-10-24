a1425a5313a986f4357ae215fe71f9cda8dccea4
DavidUKr <DavidUKr@users.noreply.github.com>
2023-10-24 18:11:10 +0300

    Song interface some methods

--- src/main/java/com/partymusicapp/Controllers/SongController.java
+++ src/main/java/com/partymusicapp/Controllers/SongController.java
@@ -1,4 +1,19 @@
 package com.partymusicapp.Controllers;
 
+import com.partymusicapp.models.Song;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("pma/api/v1/songs/{partyId}")
 public interface SongController {
+
+    @PutMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+    void addSong (@PathVariable String partyId, @RequestBody Song song);
+
+    @GetMapping(value = "/songlist", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<Song> getSongList(@PathVariable String partyId);
+
 }
