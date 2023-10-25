bbce60a9b44fd387aa819f1ab4e236d1c3d7baee
Andrei Virlan <andrevirlan@yahoo.com>
2023-10-25 21:48:25 +0300

    Added the rest of the implementations of the interfaces created in the Controller section (plus adding what is missing in interfaces)

--- src/main/java/com/partymusicapp/service/PartyService.java
+++ src/main/java/com/partymusicapp/service/PartyService.java
@@ -1,7 +1,20 @@
 package com.partymusicapp.service;
 
+import com.partymusicapp.models.dto.PartyDTO;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public interface PartyService {
+
+    PartyDTO getParty(String partyId);
+
+    void updateParty(@PathVariable String partyId);
+
+    void deleteParty(@PathVariable String partyId);
+
+    void savePartyTemplate(@PathVariable String userId, @RequestBody PartyDTO partyDTO);
+
+    void getInfo(@PathVariable String partyID);
 }
