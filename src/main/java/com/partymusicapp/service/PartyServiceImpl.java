0f452859c32334e57455435d1bfbb0bd5e691505
DavidUKr <davidkrinurs@gmail.com>
2023-10-28 22:48:05 +0300

    user advice added party not found exception

--- src/main/java/com/partymusicapp/service/PartyServiceImpl.java
+++ src/main/java/com/partymusicapp/service/PartyServiceImpl.java
@@ -1,19 +1,29 @@
 package com.partymusicapp.service;
 
+import com.partymusicapp.advice.exception.PartyNotFoundException;
 import com.partymusicapp.models.Party;
 import com.partymusicapp.models.dto.PartyDTO;
+import com.partymusicapp.models.mapper.PartyMapper;
+import com.partymusicapp.repository.PartyRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class PartyServiceImpl implements PartyService{
 
+    PartyRepo partyRepo;
+    PartyMapper partyMapper;
+
     public Party getParty(String partyId){
-        return null;
+        return getPartyOptionalById(partyId).orElseThrow(() -> new PartyNotFoundException("Party with Id "+partyId+" not found."));
     }
 
     @Override
     public PartyDTO getPartyDTO(String partyId) {
-        return null;
+        return partyMapper.partyToPartyDTO(getParty(partyId));
     }
 
     @Override
@@ -34,5 +44,9 @@ public class PartyServiceImpl implements PartyService{
     @Override
     public void getInfo(String partyID) {
 
+    }
+
+    private Optional<Party> getPartyOptionalById(String partyId) {
+        return Optional.ofNullable(partyRepo.findPartyById(partyId));
     }
 }
