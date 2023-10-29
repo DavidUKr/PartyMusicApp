0e8d9c485dbad77533a187c34eb3c8ceb669b3d0
DavidUKr <davidkrinurs@gmail.com>
2023-10-29 03:06:46 +0300

    get encoding method - not implemented

--- src/main/java/com/partymusicapp/security/SecurityConfig.java
+++ src/main/java/com/partymusicapp/security/SecurityConfig.java
@@ -3,11 +3,17 @@ package com.partymusicapp.security;
 import com.partymusicapp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -27,9 +33,22 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()
                 .and()
+                .authenticationProvider(getAuthentificationProvider())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    private static AuthenticationProvider getAuthentificationProvider() {
+        final DaoAuthenticationProvider authenticationProvider= new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(getPasswordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public static PasswordEncoder getPasswordEncoder() {
+        //return new BCryptPasswordEncoder(); //TODO implement pw encoding
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
