a44aeb40e5ea5c7fb45c7c6c69c2e4c5cc1024c3
Andrei Sebastian <andrei.virlan@student.upt.ro>
2023-11-24 09:58:42 +0200

    Implemented:
    deletePhoto, getPhoto, getAllPhotos, getUserPhotos in controller and service

--- src/main/java/com/partymusicapp/service/PhotoService.java
+++ src/main/java/com/partymusicapp/service/PhotoService.java
@@ -6,9 +6,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface PhotoService {
     @Transactional
-    public Photo savePhoto(MultipartFile file, String userId) throws IOException;
+    Photo savePhoto(MultipartFile file, String userId) throws IOException;
+
+    void deletePhoto(String photoId);
+
+    Optional<Photo> getPhoto(String photoId);
+
+    List<Photo> getAllPhotos();
+
+    List<Photo> getUserPhotos(String userId);
 }
